3ee020d455dcf4b9839d139ebcbaad4948124daa
Kimun Kim <kkm97351@gmail.com>
2021-05-16 02:27:42 +0900

    feat: 컨트롤러에서 gameId 값의 범위에 대한 검증 로직 수행 및 그에 대한 테스트 코드 작성

--- src/test/java/chess/controller/ChessApiControllerMockMvcTest.java
+++ src/test/java/chess/controller/ChessApiControllerMockMvcTest.java
@@ -1,5 +1,6 @@
 package chess.controller;
 
+
 import chess.dto.CreateGameRequest;
 import chess.dto.MoveRequest;
 import chess.service.ChessGameService;
@@ -14,13 +15,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
 @AutoConfigureMockMvc
-class ChessApiControllerMockMvcTest {
+class ChessRestControllerMockMvcTest {
     @Autowired
     private MockMvc mockMvc;
 
@@ -29,6 +32,8 @@ class ChessApiControllerMockMvcTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    private RandomGameIdGenerator randomGameIdGenerator = new RandomGameIdGenerator();
 
     @DisplayName("게임 생성 시 빈 제목의 게임을 생성 요청할 경우 400 코드를 응답한다.")
     @Test
@@ -48,12 +53,14 @@ class ChessApiControllerMockMvcTest {
                 .andExpect(status().isOk());
     }
 
-    @DisplayName("기물을 이동요청 API 테스트")
+    @DisplayName("기물 이동 요청 API의 정상 작동")
     @Test
     void movePieceTest() throws Exception {
-        String content = objectMapper.writeValueAsString(new MoveRequest(1, "a2", "a4"));
+        long gameId = randomGameIdGenerator.generateValidRandomGameId();
 
-        this.mockMvc.perform(put("/games/1/pieces")
+        String content = objectMapper.writeValueAsString(new MoveRequest(gameId, "a2", "a4"));
+        
+        this.mockMvc.perform(put("/games/" + gameId +  "/pieces")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(content))
                 .andExpect(status().isOk());
@@ -62,8 +69,26 @@ class ChessApiControllerMockMvcTest {
     @DisplayName("특정 게임 불러오기 요청 API 테스트")
     @Test
     void loadGameTest() throws Exception {
-        this.mockMvc.perform(get("/games/1"))
+        this.mockMvc.perform(get("/games/" + randomGameIdGenerator.generateValidRandomGameId()))
                 .andExpect(status().isOk());
     }
 
+    @DisplayName("유효할 수 없는 게임의 고유값이 요청되면 Bad Request를 응답한다.")
+    @Test
+    void invalidGameRequest() throws Exception {
+        long gameId = randomGameIdGenerator.generateInvalidRandomGameId();
+
+        this.mockMvc.perform(get("/games/" + gameId))
+                .andExpect(status().isBadRequest());
+    }
+
+    private static class RandomGameIdGenerator {
+        long generateValidRandomGameId() {
+            return ThreadLocalRandom.current().nextLong(Long.MAX_VALUE - 1) + 1;
+        }
+
+        long generateInvalidRandomGameId() {
+            return (-1) * ThreadLocalRandom.current().nextLong(Long.MAX_VALUE - 1) + 1;
+        }
+    }
 }
