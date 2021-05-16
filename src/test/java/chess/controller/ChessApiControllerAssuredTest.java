ca2dc0be3b6981869df44dd323d5bb78c6f0edab
Kimun Kim <kkm97351@gmail.com>
2021-05-16 21:41:35 +0900

    test: 기물 이동 API, 유효하지 않은 좌표 값에 대한 테스트 케이스 추가

--- src/test/java/chess/controller/ChessApiControllerAssuredTest.java
+++ src/test/java/chess/controller/ChessApiControllerAssuredTest.java
@@ -1,10 +1,14 @@
 package chess.controller;
 
 import chess.dto.CreateGameRequest;
+import chess.dto.MoveRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -12,6 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource("classpath:application-test.properties")
@@ -23,6 +29,7 @@ public class ChessApiControllerAssuredTest {
     private JdbcTemplate jdbcTemplate;
 
     private ObjectMapper objectMapper;
+    private RandomGameIdGenerator randomGameIdGenerator = new RandomGameIdGenerator();
 
     @BeforeEach
     void setUp() {
@@ -41,6 +48,31 @@ public class ChessApiControllerAssuredTest {
                 .when().post("/games")
                 .then().log().all()
                 .statusCode(HttpStatus.CREATED.value());
+    }
+
+
+
+    @DisplayName("기물 이동 요청시 유효하지 않은 좌표 값이 들어오면 Bad Request로 응답한다.")
+    void movePieceApiPositionValidationTest() throws Exception {
+        long gameId = randomGameIdGenerator.generateValidRandomGameId();
+
+        String content = objectMapper.writeValueAsString(new MoveRequest("aaa", "111"));
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(content)
+                .when().put("/games/" + gameId + "/pieces")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private static class RandomGameIdGenerator {
+        private static final long LOWER_BOUND = 1;
+        private static final long UPPER_BOUND = Long.MAX_VALUE;
+
+        long generateValidRandomGameId() {
+            return ThreadLocalRandom.current().nextLong(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
+        }
     }
 
     @AfterEach
