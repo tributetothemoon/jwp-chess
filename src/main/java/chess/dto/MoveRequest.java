e6e86242118650c2b35046e0b3665b7a9b4f13e2
Kimun Kim <kkm97351@gmail.com>
2021-05-16 21:13:41 +0900

    refactor: MoveRequest의 불필요 인스턴스 변수(gameId) 삭제

--- src/main/java/chess/dto/MoveRequest.java
+++ src/main/java/chess/dto/MoveRequest.java
@@ -3,7 +3,6 @@ package chess.dto;
 import javax.validation.constraints.NotEmpty;
 
 public class MoveRequest {
-    private long gameId;
     @NotEmpty
     private String from;
     @NotEmpty
@@ -12,14 +11,9 @@ public class MoveRequest {
     public MoveRequest() {
     }
 
-    public MoveRequest(long gameId, String from, String to) {
-        this.gameId = gameId;
+    public MoveRequest(String from, String to) {
         this.from = from;
         this.to = to;
-    }
-
-    public long getGameId() {
-        return gameId;
     }
 
     public String getFrom() {
@@ -28,10 +22,6 @@ public class MoveRequest {
 
     public String getTo() {
         return to;
-    }
-
-    public void setGameId(long gameId) {
-        this.gameId = gameId;
     }
 
     public void setFrom(String from) {
